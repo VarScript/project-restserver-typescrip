78a849163e29e3e7c2ff9a937ce14beca2ccc79d
DevHars <vargassanchezfabio01@gmail.com>
2022-10-18 16:51:57 -0500

    nodemon and tsc watch

--- models/serve.ts
+++ models/serve.ts
@@ -7,7 +7,7 @@ class Server {
 
     constructor(){
         this.app = express();
-        this.port = process.env.PORT || '8080';
+        this.port = process.env.PORT || '8000';
     }
 
     listen(){
