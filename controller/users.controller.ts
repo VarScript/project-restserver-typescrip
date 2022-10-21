dfaebaa70f1272d0219647d9841f33e89abbbb98
DevHars <vargassanchezfabio01@gmail.com>
2022-10-20 20:59:30 -0500

    get users

--- controller/users.controller.ts
+++ controller/users.controller.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from "express";
+import User from '../models/user';
 
 
-export const getUsers = ( req:Request, res: Response ) => {
-    res.json({
-        msg: 'getUsers'
-    })
+export const getUsers = async( req:Request, res: Response ) => {
+    const users = await User.findAll();
+    res.json({ users });
 }
 
-export const getUser = ( req:Request, res: Response ) => {
+export const getUser = async ( req:Request, res: Response ) => {
     const { id } = req.params;
-    res.json({
-        msg: 'getUser',
-        id
-    })
+    const user = await User.findByPk( id );
+    if (!user){
+        res.status(404).json({
+            msg: `The user by the Id ${id} is not exist`
+        })
+    }
+    res.json({ user })
 }
 
 export const postUser = ( req:Request, res: Response ) => {
